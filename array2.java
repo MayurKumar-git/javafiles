1301bb42793dd784bc70748975b7c31862d7a17f
Mayur Kumar <mayurkumar2704@gmail.com>
2022-06-09 14:57:13 +0530

    ANother

---
+++ array2.java
@@ -0,0 +1,13 @@
+public class array2 {
+    public static void main(String[] args){
+        String[] a= new String[3];
+        a[0]="Mayur";
+        a[1]="Chotu";
+        a[2]="Rudra";
+        for(int i=0; i<a.length; i++){
+            System.out.println(a[i]);
+        }
+
+    }
+    
+}
