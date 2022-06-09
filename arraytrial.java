9f3fc3464852c75916f6a0e6fa8353e53e720d62
Mayur Kumar <mayurkumar2704@gmail.com>
2022-06-09 14:46:21 +0530

    First

---
+++ arraytrial.java
@@ -0,0 +1,13 @@
+public class arraytrial {
+    public static void main(String args[]){
+        int a[] = new int[5];
+        a[0]=1;
+        a[1]=2;
+        a[2]=3;
+        a[4]=4;
+        for(int i=0;i<a.length;i++){//length is the property of array  
+            System.out.println(a[i]);  }
+
+    }
+    
+}
